fd2e00b8c61aeca15a61c36556761521c13e1366
Evan Putnam <quorumcode@gmail.com>
2023-10-15 23:06:47 -0700

    Initial files

---
+++ document.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+class Document:
+    def __init__(self, sudoc: str, sudoc_cleaned: str, title: str, publication_date: datetime.date):
+        self.sudoc = sudoc
+        self.sudoc_cleaned = sudoc_cleaned
+        self.title = title
+        self.publication_date = publication_date
+        return
+
+    def get_row(self) -> str:
+        return f"{self.sudoc},{self.title},{self.publication_date}"
