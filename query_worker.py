548e8df912b76b559cda5821e407bf6b99f33deb
Kv139 <89169913+Kv139@users.noreply.github.com>
2023-11-21 15:38:37 -0800

    Add files via upload

---
+++ query_worker.py
@@ -0,0 +1,20 @@
+from PyQt5.QtCore import QThread, pyqtSignal
+from ocr_lines_new3 import read_data
+
+class QueryWorker(QThread):
+    finished = pyqtSignal()
+    result_ready = pyqtSignal(str)  # Define the signal here
+    progress_updated = pyqtSignal(float)
+
+    def __init__(self, directory_path):
+        super().__init__()
+        self.directory_path = directory_path
+
+    def run(self):
+        result = read_data(self.directory_path,self.progress_updated)
+        self.result_ready.emit(result)  # Emit the signal when the result is ready
+        self.finished.emit()
+
+    #def run_query(self):
+     #   extract_query_data()
+      #  self.finished.emit()
