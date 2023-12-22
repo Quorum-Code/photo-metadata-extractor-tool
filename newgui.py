7abc992cd4e0adc23ad7b1aa27966da92a9ea595
Evan Putnam <quorumcode@gmail.com>
2023-12-22 11:41:24 -0800

    Multi-page gui

--- newgui.py
+++ newgui.py
@@ -23,6 +23,112 @@ def long_process():
         time.sleep(.5)
 
 
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # Window settings
+        self.title("Photo Metadata Extractor Tool")
+        self.geometry("800x600")
+
+        # Side button setup
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        # Navigation bar
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(3, weight=1)
+
+        # Navigation buttons
+        self.home_button = customtkinter.CTkButton(self.navigation_frame,
+                                                   corner_radius=0,
+                                                   height=40,
+                                                   border_spacing=10,
+                                                   text="Home",
+                                                   fg_color="transparent",
+                                                   anchor="w",
+                                                   command=self.home_button_event)
+        self.home_button.grid(row=0, column=0, sticky="ew")
+
+        self.settings_button = customtkinter.CTkButton(self.navigation_frame,
+                                                   corner_radius=0,
+                                                   height=40,
+                                                   border_spacing=10,
+                                                   text="Settings",
+                                                   fg_color="transparent",
+                                                   anchor="w",
+                                                   command=self.settings_button_event)
+        self.settings_button.grid(row=1, column=0, sticky="ew")
+
+        self.configuration_button = customtkinter.CTkButton(self.navigation_frame,
+                                                   corner_radius=0,
+                                                   height=40,
+                                                   border_spacing=10,
+                                                   text="Configuration",
+                                                   fg_color="transparent",
+                                                   anchor="w",
+                                                   command=self.configuration_button_event)
+        self.configuration_button.grid(row=2, column=0, sticky="ew")
+
+        # Style menu
+        self.style_menu = customtkinter.CTkOptionMenu(self.navigation_frame,
+                                                      values=["Light", "Dark", "System"])
+        self.style_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+
+        # Initialize Home frame
+        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.home_frame.grid_columnconfigure(0, weight=1)
+        self.home_text = customtkinter.CTkLabel(self.home_frame, text="Home page...")
+        self.home_text.grid(row=0, column=0, padx=20, pady=20)
+
+        # Initialize Settings frame
+        self.settings_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.settings_text = customtkinter.CTkLabel(self.settings_frame, text="Settings page...")
+        self.settings_text.grid(row=0, column=0, padx=20, pady=20)
+
+        # Initialize Configuration frame
+        self.configuration_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+
+        # Initialize Info frame
+        self.info_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+
+        # Load home frame
+        self.select_frame_by_name("home")
+
+    def select_frame_by_name(self, name: str):
+        # Update navigation button colors
+        self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
+        self.settings_button.configure(fg_color=("gray75", "gray25") if name == "settings" else "transparent")
+        self.configuration_button.configure(fg_color=("gray75", "gray25") if name == "configuration" else "transparent")
+
+        # Set or forget frames
+        if name == "home": self.home_frame.grid(row=0, column=1, sticky="nsew")
+        else: self.home_frame.grid_forget()
+
+        if name == "settings": self.settings_frame.grid(row=0, column=1, sticky="nsew")
+        else: self.settings_frame.grid_forget()
+
+        if name == "configuration": self.configuration_frame.grid(row=0, column=1, sticky="nsew")
+        else: self.configuration_frame.grid_forget()
+
+        if name == "info": self.configuration_frame.grid(row=0, column=1, sticky="nsew")
+        else: self.info_frame.grid_forget()
+
+
+    def home_button_event(self):
+        self.select_frame_by_name("home")
+
+    def settings_button_event(self):
+        self.select_frame_by_name("settings")
+
+    def configuration_button_event(self):
+        self.select_frame_by_name("configuration")
+
+    def info_button_event(self):
+        self.select_frame_by_name("info")
+
+
 class GUI:
     def __init__(self, light: bool = True):
         # Graphic Theme
@@ -79,4 +185,5 @@ class GUI:
         self.progressbar.set(self.progress)
 
 if __name__ == "__main__":
-    main()
+    app = App()
+    app.mainloop()
