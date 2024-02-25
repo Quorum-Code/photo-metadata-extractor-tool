ff1023150aaa4afb59633d86f9ef319062b8ccdb
Evan Putnam <quorumcode@gmail.com>
2024-02-25 15:23:56 -0800

    application_window.py - Updated comments extraction_page.py - Refactored from home_page.py getting_started_page.py - New page with information file_handler.py - Removed print statements page.py - Created parent class for pages

--- gui/application_window.py
+++ gui/application_window.py
@@ -1,5 +1,5 @@
 import customtkinter
-from gui import home_page, settings_page, config_page
+from gui import extraction_page, settings_page, config_page, getting_started_page
 from file_handler import FileHandler
 
 
@@ -21,20 +21,34 @@ class App(customtkinter.CTk):
         ************** """
         self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
-        self.navigation_frame.grid_rowconfigure(3, weight=1)
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
 
-        # Navigation buttons
+        # NAVIGATION BUTTONS
+        # - Getting Started
+        self.__getting_started_button = customtkinter.CTkButton(self.navigation_frame,
+                                                                corner_radius=0,
+                                                                height=40,
+                                                                border_spacing=10,
+                                                                text="Getting Started",
+                                                                fg_color="transparent",
+                                                                text_color=("gray10", "gray90"),
+                                                                anchor="w",
+                                                                command=self.__getting_started_button_event)
+        self.__getting_started_button.grid(row=0, column=0, sticky="ew")
+
+        # - Extraction
         self.home_button = customtkinter.CTkButton(self.navigation_frame,
                                                    corner_radius=0,
                                                    height=40,
                                                    border_spacing=10,
-                                                   text="Home",
+                                                   text="Extraction",
                                                    fg_color="transparent",
                                                    text_color=("gray10", "gray90"),
                                                    anchor="w",
-                                                   command=self.home_button_event)
-        self.home_button.grid(row=0, column=0, sticky="ew")
+                                                   command=self.__extraction_button_event)
+        self.home_button.grid(row=1, column=0, sticky="ew")
 
+        # - Settings
         self.settings_button = customtkinter.CTkButton(self.navigation_frame,
                                                        corner_radius=0,
                                                        height=40,
@@ -44,8 +58,9 @@ class App(customtkinter.CTk):
                                                        text_color=("gray10", "gray90"),
                                                        anchor="w",
                                                        command=self.settings_button_event)
-        self.settings_button.grid(row=1, column=0, sticky="ew")
+        self.settings_button.grid(row=2, column=0, sticky="ew")
 
+        # - Configuration
         self.configuration_button = customtkinter.CTkButton(self.navigation_frame,
                                                             corner_radius=0,
                                                             height=40,
@@ -55,7 +70,7 @@ class App(customtkinter.CTk):
                                                             text_color=("gray10", "gray90"),
                                                             anchor="w",
                                                             command=self.configuration_button_event)
-        self.configuration_button.grid(row=2, column=0, sticky="ew")
+        self.configuration_button.grid(row=3, column=0, sticky="ew")
 
         # Style menu
         self.style_menu = customtkinter.CTkOptionMenu(self.navigation_frame,
@@ -66,23 +81,26 @@ class App(customtkinter.CTk):
         self.change_style_event(style)
         self.style_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
 
-        # Initialize Home frame
-        self.home = home_page.HomePage(self, self.filehandler)
+        # INITIALIZING PAGES
+        # - Initialize Getting Started frame
+        self.__getting_started = getting_started_page.GettingStartedPage(self)
+
+        # - Initialize Home frame
+        self.__extraction_page = extraction_page.ExtractionPage(self, self.filehandler)
         self.process_folder = "None"
         self.sudoc_file = "None"
 
-        # Initialize Settings frame
+        # - Initialize Settings frame
         self.settings = settings_page.SettingsPage(self, self.filehandler)
 
-        # Initialize Configuration frame
+        # - Initialize Configuration frame
         self.configuration = config_page.ConfigurationPage(self, self.filehandler)
 
-        # Initialize Info frame
+        # - Initialize Info frame
         self.info_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
 
         # Load home frame
-        self.select_frame_by_name("home")
-
+        self.select_frame_by_name("getting_started")
         self.grab_set()
 
     def select_frame_by_name(self, name: str):
@@ -95,10 +113,15 @@ class App(customtkinter.CTk):
         self.focus()
 
         # Set or forget frames
-        if name == "home":
-            self.home.home_frame.grid(row=0, column=1, sticky="nsew")
+        if name == "getting_started":
+            self.__getting_started.frame.grid(row=0, column=1, sticky="nsew")
         else:
-            self.home.home_frame.grid_forget()
+            self.__getting_started.frame.grid_forget()
+
+        if name == "extraction":
+            self.__extraction_page.home_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.__extraction_page.home_frame.grid_forget()
 
         if name == "settings":
             self.settings.settings_frame.grid(row=0, column=1, sticky="nsew")
@@ -115,8 +138,11 @@ class App(customtkinter.CTk):
         else:
             self.info_frame.grid_forget()
 
-    def home_button_event(self):
-        self.select_frame_by_name("home")
+    def __getting_started_button_event(self):
+        self.select_frame_by_name("getting_started")
+
+    def __extraction_button_event(self):
+        self.select_frame_by_name("extraction")
 
     def settings_button_event(self):
         self.select_frame_by_name("settings")
@@ -128,6 +154,5 @@ class App(customtkinter.CTk):
         self.select_frame_by_name("info")
 
     def change_style_event(self, style: str):
-        print(f"NEW STYLE: {style}")
         customtkinter.set_appearance_mode(style)
         self.filehandler.save_style(style)
