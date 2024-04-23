5bebb038ca599fd5572386cb5502bf86037cc8ef
Evan Putnam <quorumcode@gmail.com>
2024-04-23 10:36:13 -0700

    csv_reader file

---
+++ csv_reader.py
@@ -0,0 +1,18 @@
+import csv
+
+
+class CSVReader:
+    def __init__(self):
+        self.__rows: list[dict] = []
+        return
+
+    def get_data(self) -> list[dict]:
+        return self.__rows
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
