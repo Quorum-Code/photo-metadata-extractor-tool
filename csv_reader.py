e15f5e60a38ee8bdaaa646b14926d58bb912b9a8
Evan Putnam <quorumcode@gmail.com>
2024-04-23 11:55:44 -0700

    small test for csv_reader

--- csv_reader.py
+++ csv_reader.py
@@ -1,16 +1,47 @@
 import csv
+import os
 
 
 class CSVReader:
-    def __init__(self):
-        self.__rows: list[dict] = []
+    DELIMITER = "|"
+
+    def __init__(self, path):
+        self.__path: str = path
+        self.__col_names: list[str] = []
+        self.__rows: list = []
+
+        self.__read_data()
+        return
+
+    def __read_data(self):
+        if self.__path == "":
+            return
+
+        if not os.path.exists(self.__path):
+            return
+
+        with open(self.__path, mode='r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter='|')
+            for row in csv_reader:
+                self.__rows.append(row)
+
+        self.__col_names = self.__rows[0]
+        self.__rows = self.__rows[1:]
+
         return
 
     def get_data(self) -> list[dict]:
         return self.__rows
 
+    def get_col_names(self) -> list[str]:
+        return self.__col_names
+
 
 def main():
+    path = "./extractions/extraction_02-18-2024_13.46.13.csv"
+    cr = CSVReader(path)
+    print(cr.get_col_names())
+    print(cr.get_data())
     return
 
 
